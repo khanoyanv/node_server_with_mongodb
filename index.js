1721d93df3f61cc886ff0b7b06d7f1f6b3cd3da5
Vahe <khanoyanv@gmail.com>
2024-08-29 20:13:05 +0400

    getting data from db and rendering on front end

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-import axios from "axios";
-import express from "express";
-import cors from "cors";
-
-const app = express();
-
-async function getHandler() {
-    const products = await axios.get("localhost:8081/products");
-    console.log(products.response);
-}
-
-async function postHandler() {
-    const name = document.getElementById("input-name").value;
-    const price = document.getElementById("input-price").value;
-    console.log(name);
-    console.log(price);
-    // let res = await axios.post("localhost:8081/products");
+function fetchContent() {
+    const url = 'http://135.181.37.152:3029/products';
+    fetch(url)
+    .then(res => {
+        if(!res.ok) {
+            throw new Error('Network response was not ok ' + response.statusTex)
+        }
+        return res.json();
+    })
+    .then(data => {
+        let main = document.getElementById("main");
+        data.forEach(product => {
+            const card = document.createElement('div');
+            card.className = 'card mb-3';
+            card.style.maxWidth = '18rem';
+            card.innerHTML = `
+            <img src="${product.imageUrl}" class="card-img-top" alt="${product.name}" style="height: 200px; object-fit: cover;">
+            <div class="card-body">
+                <h5 class="card-title">${product.name}</h5>
+                <p class="card-text">$${product.price}</p>
+                <a href="#" class="btn btn-primary">Go to Product Page</a>
+            </div>
+            `;
+            main.appendChild(card);
+        })                    
+        console.log(main);
+    })
 }
